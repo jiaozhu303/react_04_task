672a1602d1c709adb4bc236445b8b57bd4daecc3
Dongjie <zhaodj5@lenovo.com>
2017-03-21 11:30:27 +0800

    layout page

--- components/App.js
+++ components/App.js
@@ -1,8 +1,20 @@
 'use strict'
 import React, {Component} from 'react';
+import './AppStyle.css'
 
 export default class App extends Component {
-    render(){
-        return (<h1> hello React !!!</h1>);
+    render() {
+        return ( <div className="content">
+                    <article>
+                        <h1> 首页</h1>
+                    </article>
+                    <footer>
+                        <ul>
+                            <li> 首页</li>
+                            <li> 消息</li>
+                        </ul>
+                    </footer>
+                </div>
+        );
     }
 }
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM, {render} from 'react-dom';
 import {Router, Route, hashHistory, IndexRoute, Redirect, browserHistory} from 'react-router';
 import App from './components/App';
+import './components/indexStyle.css'
 
 
 const router = (
