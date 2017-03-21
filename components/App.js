247cc9c6893d6891e198d7ec7b47acaa2a85b3c0
Dongjie <zhaodj5@lenovo.com>
2017-03-21 10:44:15 +0800

    init project file

---
+++ components/App.js
@@ -0,0 +1,8 @@
+'use strict'
+import React, {Component} from 'react';
+
+export default class App extends Component {
+    render(){
+        return (<h1> hello React !!!</h1>);
+    }
+}
---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM, {render} from 'react-dom';
+import {Router, Route, hashHistory, IndexRoute, Redirect, browserHistory} from 'react-router';
+import App from './components/App';
+
+
+const router = (
+    <Router history={browserHistory}>
+        <Route path="/" components={App}>
+            <IndexRoute components={App}/>
+
+        </Route>
+    </Router>
+);
+
+ReactDOM.render(router, document.getElementById('app'));
